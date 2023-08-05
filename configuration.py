ed22d86ce3746673cf21275e1af9b99a881201eb
KateKaplan <ekaterinakaplan@yandex.ru>
2023-08-05 11:51:48 +0500

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://2d8ee86e-76d1-4a99-b7f1-27249632b2d8.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
